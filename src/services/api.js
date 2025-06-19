72d74da7dadbc09fddbbb8403316abcb49c0e3a0
Abhishek-Yadav17 <17abhishekadav@gmail.com>
2025-06-19 14:38:39 +0530

    url changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://theglenbackend.onrender.com',
+  baseURL: 'https://theglenbackend.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
